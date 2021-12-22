dd3b228637fe331c5cca65757a319dc122d6cb5a
blonsubash <subash.lama100@gmail.com>
2021-12-22 19:50:45 +0545

    Dropdown Selection of Categories Completed

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,16 @@
+var productLists = [];
+var spinnerWrapper = document.querySelector(".spinner-border");
+var table = document.getElementById("productListTable");
+
 axios.get("https://fakestoreapi.com/products").then((response) => {
   myProductList(response.data);
+  spinnerWrapper.style.display = "none";
 });
 
 function myProductList(data) {
-  var table = document.getElementById("productListTable");
-
   for (var i = 0; i < data.length; i++) {
+    productLists.push(data[i]);
+
     var row = `<tr>
                     <td>${data[i].id}</td>
                     <td>${data[i].title}</td>
@@ -16,8 +21,82 @@ function myProductList(data) {
         </tr>`;
     table.innerHTML += row;
   }
+  console.log("Data", productLists);
 }
-// let spinnerWrapper = document.querySelector(".spinner-border");
-// window.addEventListener("load", function () {
-//   spinnerWrapper.style.display = "none";
-// });
+
+function selectCategory() {
+  if (this.value == "mensclothing") {
+    productLists = [];
+    table.innerHTML = "";
+    axios
+      .get(`https://fakestoreapi.com/products/category/men's clothing`)
+      .then((response) => {
+        myProductList(response.data);
+      });
+  }
+  if (this.value == "jewerly") {
+    productLists = [];
+    table.innerHTML = "";
+    axios
+      .get(`https://fakestoreapi.com/products/category/jewelery`)
+      .then((response) => {
+        myProductList(response.data);
+      });
+  }
+  if (this.value == "electronics") {
+    productLists = [];
+    table.innerHTML = "";
+    axios
+      .get(`https://fakestoreapi.com/products/category/electronics`)
+      .then((response) => {
+        myProductList(response.data);
+      });
+  }
+  if (this.value == "womensclothing") {
+    productLists = [];
+    table.innerHTML = "";
+    axios
+      .get(`https://fakestoreapi.com/products/category/women's clothing`)
+      .then((response) => {
+        myProductList(response.data);
+      });
+  }
+  if (this.value == "allcategories") {
+    productLists = [];
+    table.innerHTML = "";
+    axios.get(`https://fakestoreapi.com/products`).then((response) => {
+      myProductList(response.data);
+    });
+  }
+}
+document.getElementById("selection").addEventListener("change", selectCategory);
+
+// function searchMyProductLists(searchString) {
+//   let searchResult = productLists.filter(function (item, index) {
+//     return (
+//       item.id === searchString ||
+//       item.title === searchString ||
+//       item.price === searchString ||
+//       item.category === searchString ||
+//       item.description === searchString
+//     );
+//   });
+//   return searchResult;
+// }
+
+// document
+//   .getElementById("searchProductListsBtn")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const searchResult = document.getElementById("searchText").value;
+//     console.log(searchResult);
+//     let finalResult = searchMyProductLists(searchResult);
+//     if (finalResult.length > 0) {
+//       console.log("FInal result", finalResult);
+//       productLists = [];
+//       table.innerHTML = "";
+//       myProductList(finalResult);
+//     } else {
+//       alert("No data found");
+//     }
+//   });
