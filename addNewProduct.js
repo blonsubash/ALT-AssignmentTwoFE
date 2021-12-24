462e570d008dedc5b5613f8761aeb64a54da3db5
blonsubash <subash.lama100@gmail.com>
2021-12-24 16:24:44 +0545

    Popup Completed

--- addNewProduct.js
+++ addNewProduct.js
@@ -1,5 +1,3 @@
-console.log("Hello");
-
 function addNewProduct(e) {
   e.preventDefault();
   var newProductName = document.getElementById("newItemName").value;
@@ -8,12 +6,6 @@ function addNewProduct(e) {
     document.getElementById("newItemDescription").value;
   var newProductImage = document.getElementById("newItemImage").value;
   var newProductCategory = document.getElementById("selection").value;
-
-  console.log("name", newProductName);
-  console.log("price", newProductPrice);
-  console.log("detail", newProductDescription);
-  console.log("img", newProductImage);
-  console.log("category", newProductCategory);
 
   function checkProductName() {
     if (newProductName === "") {
@@ -59,6 +51,46 @@ function addNewProduct(e) {
       return true;
     }
   }
+
+  function handleSuccess() {
+    document.getElementById("newItemName").value = "";
+    document.getElementById("newItemPrice").value = "";
+    document.getElementById("newItemDescription").value = "";
+    document.getElementById("newItemImage").value = "";
+    document.getElementById("selection").value = "selectCategory";
+    document.getElementById("newNameError").innerHTML = "";
+    document.getElementById("newPriceError").innerHTML = "";
+    document.getElementById("newDescriptionError").innerHTML = "";
+    document.getElementById("newImageError").innerHTML = "";
+    document.getElementById("newSelectionError").innerHTML = "";
+    document.getElementById("newItemName").style.borderColor = "#e6e6e6";
+    document.getElementById("newItemPrice").style.borderColor = "#e6e6e6";
+    document.getElementById("newItemDescription").style.borderColor = "#e6e6e6";
+    document.getElementById("newItemImage").style.borderColor = "#e6e6e6";
+    document.getElementById("newItemImage").style.borderColor = "#e6e6e6";
+    document.getElementById("dataUploadedPopup").style.display = "block";
+    document.getElementById("addNewItemBtn").innerHTML = "Add Item";
+    document.getElementById("confirmBtn").style.cursor = "pointer";
+  }
+
+  function handlePopupClose() {
+    document
+      .getElementById("confirmBtn")
+      .addEventListener("click", function () {
+        window.location.href = "homepage1.html";
+        document.getElementById("dataUploadedPopup").style.display = "none";
+      });
+  }
+  function handleProcessingTime() {
+    document.getElementById("newItemName").style.cursor = "not-allowed";
+    document.getElementById("addNewItemBtn").style.cursor = "not-allowed";
+    document.getElementById("newItemPrice").style.cursor = "not-allowed";
+    document.getElementById("newItemDescription").style.cursor = "not-allowed";
+    document.getElementById("newItemImage").style.cursor = "not-allowed";
+    document.getElementById("selection").style.cursor = "not-allowed";
+    document.getElementById("addNewItemBtn").innerHTML =
+      "Processing Please Wait...";
+  }
   var checkProductNameResult = checkProductName();
   var checkProductPriceResult = checkProductPrice();
   var checkProductDescriptionResult = checkProductDescription();
@@ -72,6 +104,8 @@ function addNewProduct(e) {
     checkProductImageResult &&
     checkProductCategoryResult
   ) {
+    handleProcessingTime();
+
     axios
       .post(
         "https://fakestoreapi.com/products",
@@ -87,28 +121,8 @@ function addNewProduct(e) {
       .then((res) => {
         if (res.status === 200) {
           console.log("New Item Added Successfully");
-          // window.location.href = "homepage1.html";
-          document.getElementById("newItemName").value = "";
-          document.getElementById("newItemPrice").value = "";
-          document.getElementById("newItemDescription").value = "";
-          document.getElementById("newItemImage").value = "";
-          document.getElementById("selection").value = "selectCategory";
-          document.getElementById("newNameError").innerHTML = "";
-          document.getElementById("newPriceError").innerHTML = "";
-          document.getElementById("newDescriptionError").innerHTML = "";
-          document.getElementById("newImageError").innerHTML = "";
-          document.getElementById("newSelectionError").innerHTML = "";
-
-          document.getElementById("newItemName").style.borderColor = "#e6e6e6";
-
-          document.getElementById("newItemPrice").style.borderColor = "#e6e6e6";
-
-          document.getElementById("newItemDescription").style.borderColor =
-            "#e6e6e6";
-
-          document.getElementById("newItemImage").style.borderColor = "#e6e6e6";
-
-          document.getElementById("newItemImage").style.borderColor = "#e6e6e6";
+          handleSuccess();
+          handlePopupClose();
         }
       })
       .catch((err) => console.error(err));
---
+++ commonFunction.js
@@ -0,0 +1,25 @@
+// var modal = document.getElementById("myModal");
+// if (modal) {
+//   function uploadDataPopup() {
+//     modal.style.display = "block";
+//   }
+//   function closePopup() {
+//     modal.style.display = "none";
+//   }
+//   //   window.onclick = function (event) {
+//   //     if (event.target == modal) {
+//   //       modal.style.display = "none";
+//   //     }
+//   //   };
+// }
+// if (document.getElementById("addNewItemBtn")) {
+//   document
+//     .getElementById("addNewItemBtn")
+//     .addEventListener("click", uploadDataPopup);
+// }
+
+// if (document.getElementsByClassName("close")[0]) {
+//   document
+//     .getElementsByClassName("close")[0]
+//     .addEventListener("click", closePopup);
+// }
